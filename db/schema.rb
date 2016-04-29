7af09276214fa6c1fa455663686b28cf28b17ba8
Owais Wiz <owaiswizard@gmail.com>
2016-04-30 01:46:58 +0530

    Completed adding suitable mailer for confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410140636) do
+ActiveRecord::Schema.define(version: 20160429201343) do
 
   create_table "instances", force: :cascade do |t|
     t.integer  "user_id"
@@ -44,10 +44,12 @@ ActiveRecord::Schema.define(version: 20160410140636) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
