79eb513d6cb6e395488d2411807c032a0c29d1fd
Owais Wiz <owaiswizard@gmail.com>
2016-04-29 22:40:56 +0530

    Cloned

--- db/schema.rb
+++ db/schema.rb
@@ -44,12 +44,10 @@ ActiveRecord::Schema.define(version: 20160410140636) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.string   "username"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
