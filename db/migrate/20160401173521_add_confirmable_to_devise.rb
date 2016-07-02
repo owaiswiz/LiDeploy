ef3e304c2dd663d3548f701a60a2cb353ee1a94b
Owais Wiz <owaiswizard@gmail.com>
2016-07-02 23:51:46 +0530

    Changed AddConfirmabletoDevise to work with mysql

--- db/migrate/20160401173521_add_confirmable_to_devise.rb
+++ db/migrate/20160401173521_add_confirmable_to_devise.rb
@@ -7,11 +7,11 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     # User.reset_column_information # Need for some types of updates, but not for update_all.
     # To avoid a short time window between running the migration and updating all existing
     # users as confirmed, do the following
-    execute("UPDATE users SET confirmed_at = datetime()")
+    #execute("UPDATE users SET confirmed_at = datetime()")
     # All existing user accounts should be able to log in after this.
     # Remind: Rails using SQLite as default. And SQLite has no such function :NOW.
     # Use :date('now') instead of :NOW when using SQLite.
-    # => execute("UPDATE users SET confirmed_at = date('now')")
+    execute("UPDATE users SET confirmed_at = date('now')")
     # Or => User.all.update_all confirmed_at: Time.now
   end
 
