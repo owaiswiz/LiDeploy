c218e39cb4482821380589fc4794b34efc3e0fdd
Owais Wiz <owaiswizard@gmail.com>
2016-07-03 00:59:19 +0530

    Set config.assets.compile to false

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '2.0'
 
 %w( home instances registrations sessions account pages tickets records admin ).each do |controller|
   Rails.application.config.assets.precompile += ["#{controller}.js.coffee","#{controller}.js","#{controller}.scss","#{controller}.css"]
