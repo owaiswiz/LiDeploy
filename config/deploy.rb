abd7cbc3e81ad0485b5b5f7ee218ef295fb9507a
Owais <owaiswiz@gmail.com>
2017-02-05 15:12:46 +0530

    Update Deploy IP

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://owaiswiz:Owaiskhan1@github.com/owaiswiz/host.git'
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/deploy/lideploy'
+set :deploy_to, '/home/ubuntu/lideploy'
 # Default value for :scm is :git
 # set :scm, :git
 
